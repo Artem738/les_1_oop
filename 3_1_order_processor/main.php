bf8f70f4f48d84d8a78c199aa55f4e215b5dded5
ArtRyzen <artembabichev@gmail.com>
2023-06-28 15:30:30 +0300

    New version of Order

---
+++ 3_1_order_processor/main.php
@@ -0,0 +1,137 @@
+<?php
+
+class Order
+{
+    public function __construct(
+        protected int    $id,
+        protected string $type,
+        protected string $data
+    ) {
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): void
+    {
+        $this->type = $type;
+    }
+
+    public function getData(): string
+    {
+        return $this->data;
+    }
+
+    public function setData(string $data): void
+    {
+        $this->data = $data;
+    }
+}
+
+class OrderProcessor
+{
+    private OrderHandlerInterface $orderHandler;     // Обробник замовлення
+    private ReportGeneratorInterface $reportGenerator;  // Генератор звіту
+
+    public function setOrderHandler(OrderHandlerInterface $orderHandler): void
+    {
+        $this->orderHandler = $orderHandler;
+    }
+
+    public function setReportGenerator(ReportGeneratorInterface $reportGenerator): void
+    {
+        $this->reportGenerator = $reportGenerator;
+    }
+
+    public function processOrder(Order $order): void
+    {
+        // Обробка замовлення за допомогою обробника
+        $this->orderHandler->processOrder($order);
+        // Генерація звіту за допомогою генератора
+        $this->reportGenerator->generateReport($order);
+    }
+
+    public function displayOrderInfo(Order $order): void
+    {
+        echo 'Інформація про замовлення:' . PHP_EOL;
+        echo 'ID: ' . $order->getId() . PHP_EOL;
+        echo 'Тип: ' . $order->getType() . PHP_EOL;
+        echo 'Дані: ' . $order->getData() . PHP_EOL;
+    }
+}
+
+interface OrderHandlerInterface
+{
+    public function processOrder(Order $order): void;
+}
+
+class ProductOrderHandler implements OrderHandlerInterface
+{
+    public function processOrder(Order $order): void
+    {
+        echo "Обробка замовлення №" . $order->getId() . " на товар з типом " . $order->getType() . PHP_EOL;
+    }
+}
+
+class ServiceOrderHandler implements OrderHandlerInterface
+{
+    public function processOrder(Order $order): void
+    {
+        echo "Обробка замовлення №" . $order->getId() . " на послугу з типом " . $order->getType() . PHP_EOL;
+    }
+}
+
+class DeliveryOrderHandler implements OrderHandlerInterface
+{
+    public function processOrder(Order $order): void
+    {
+        echo "Обробка замовлення на доставку з типом " . $order->getType() . PHP_EOL;
+    }
+}
+
+interface ReportGeneratorInterface
+{
+    public function generateReport(Order $order): void;
+}
+
+class PDFReportGenerator implements ReportGeneratorInterface
+{
+    public function generateReport(Order $order): void
+    {
+        echo 'Звіт - №' . $order->getId() . ' - відображено у форматі PDF.' . PHP_EOL;
+    }
+}
+
+class CSVReportGenerator implements ReportGeneratorInterface
+{
+    public function generateReport(Order $order): void
+    {
+        echo 'Звіт - №' . $order->getId() . ' - відображено у форматі CSV.' . PHP_EOL;
+    }
+}
+
+$order = new Order(1, 'product', 'Дані про замовлення на товар');
+
+// Створюємо об'єкт обробника замовлення та генератора звіту
+$orderHandler = new ProductOrderHandler();
+$reportGenerator = new PDFReportGenerator();
+
+// Створюємо об'єкт процесора замовлення
+$orderProcessor = new OrderProcessor();
+
+// Налаштовуємо процесор замовлення з обраними обробником та генератором звіту
+$orderProcessor->setOrderHandler($orderHandler);
+$orderProcessor->setReportGenerator($reportGenerator);
+
+// Обробляємо замовлення
+$orderProcessor->processOrder($order);
+
+// Виводимо інформацію про замовлення
+$orderProcessor->displayOrderInfo($order);
